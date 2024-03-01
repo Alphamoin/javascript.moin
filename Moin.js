53ab2a4778a4b5a81b4b460fc1dbc58af367aed4
Alphamoin <158542262+Alphamoin@users.noreply.github.com>
2024-03-01 06:00:23 +0000

    Pending changes exported from your codespace

---
+++ Moin.js
@@ -0,0 +1,20 @@
+let score = "moin"
+
+console.log (typeof score);
+console .log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let  IsLoggedIn = "MOIN"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber =7
+
+let stringNumber= string(someNumber)
+
+console.log(stringNumber);
+
---
+++ alpha.js
@@ -0,0 +1,20 @@
+let value = 7
+let negvalue =-value
+console.log(negvalue);
+
+//console.log (2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+// console.log=(2%3);
+
+let str1 = "Moin"
+let str2 = " shaikh"
+let str3= str1 +str2
+
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+conaole.lpg(1+2+"2")
+)
